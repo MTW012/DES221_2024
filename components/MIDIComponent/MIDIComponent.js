6e00b4159699d818cc8b06f93167a659e638bd53
dr-offig <dr.offig@offig.com>
2023-02-18 15:29:44 +1000

    Made Custom Midi Web Component

---
+++ components/MIDIComponent/MIDIComponent.js
@@ -0,0 +1,146 @@
+// A utility function for creating a new html element with given id and class
+const newElement = (tag, id, clsName) => {
+  const elem = document.createElement(tag);
+  elem.className = clsName;
+  elem.id = id;
+  return elem;
+}
+
+// handy function for checking whether a variable is a valid non-empty array
+// const arrayOK = (x) => {
+//   return x && Array.isArray(x) && x.length;
+// }
+
+
+// This is our custom web component, which implements MIDI port access
+class CustomMIDI extends HTMLElement {
+  
+    constructor() {
+      // Always call super first in constructor
+      super();
+      
+      // get access to the DOM tree for this element
+      const shadow = this.attachShadow({mode: 'open'});
+      
+      // Apply customMidi external stylesheet to the shadow dom
+      const linkElem = document.createElement('link');
+      linkElem.setAttribute('rel', 'stylesheet');
+      linkElem.setAttribute('href', 'components/MIDIComponent/MIDIComponent.css');
+
+      // Attach the created elements to the shadow dom
+      shadow.appendChild(linkElem);
+
+      // Create a top level panel
+      this.mainPanel = newElement('div', 'customMidiMainPanel', 'custom-midi main-panel horizontal-panel');
+      shadow.appendChild(this.mainPanel);
+
+      // Populate an input and output port table, with toggle buttons to connect/disconnect
+      this.inputPortTable = newElement('div', 'customMidiInputPortTable', 'port-table vertical-panel');
+      this.mainPanel.appendChild(this.inputPortTable);
+      this.inputPortTableHeader = newElement('div', 'customMidiInputPortTableHeader', 'port-table-header');
+      this.inputPortTableHeader.innerHTML = 'MIDI Inputs';
+      this.inputPortTable.appendChild(this.inputPortTableHeader);
+
+      this.outputPortTable = newElement('div', 'customMidiOutputPortTable', 'port-table vertical-panel');
+      this.mainPanel.appendChild(this.outputPortTable);
+      this.outputPortTableHeader = newElement('div', 'customMidiOutputPortTableHeader', 'port-table-header');
+      this.outputPortTableHeader.innerHTML = 'MIDI Outputs';
+      this.outputPortTable.appendChild(this.outputPortTableHeader);
+
+    }
+  
+    populatePortTables() {
+      if (this.midi) {
+        if (this.midi.inputs) {
+          this.midi.inputs.forEach((inputPort) => {
+            // a panel to hold the name of the port, and a toggle button for connecting
+            const inputPortPanel = newElement('div', `inputPortPanel_${inputPort.id}`, 'port-panel horizontal-panel'); 
+            this.inputPortTable.appendChild(inputPortPanel);
+            // the name of the midi port
+            const inputPortName = newElement('div', `customMidiInputPortName_${inputPort.id}`, 'port-name');
+            inputPortName.innerHTML = `${inputPort.manufacturer} ${inputPort.name}`;
+            inputPortPanel.appendChild(inputPortName);
+            // toggle button for connecting
+            const inputPortToggle = newElement('button', `inputPortToggle_${inputPort.id}`, 'port-toggle toggled-off');
+            inputPortToggle.innerHTML = "Connect";
+            inputPortPanel.appendChild(inputPortToggle);
+            if (inputPort.connection == 'open') {
+              inputPortToggle.classList.remove('toggled-off');
+              inputPortToggle.classList.add('toggled-on');
+              inputPortToggle.innerHTML = "Diconnect";
+            }
+            inputPortToggle.addEventListener('click', async (event) => {
+              if (inputPortToggle.classList.contains('toggled-on')) {
+                await inputPort.close();
+                inputPortToggle.classList.remove('toggled-on');
+                inputPortToggle.classList.add('toggled-off');
+                inputPortToggle.innerHTML = 'Connect';
+              } else {
+                await inputPort.open();
+                inputPortToggle.classList.remove('toggled-off');
+                inputPortToggle.classList.add('toggled-on');
+                inputPortToggle.innerHTML = 'Disconnect';
+              }          
+            });
+          });
+        }
+      
+        if (this.midi.outputs) {
+          this.midi.outputs.forEach((outputPort) => {
+            // a panel to hold the name of the port, and a toggle button for connecting
+            const outputPortPanel = newElement('div', `outputPortPanel_${outputPort.id}`, 'port-panel horizontal-panel'); 
+            this.outputPortTable.appendChild(outputPortPanel);
+            // the name of the midi port
+            const outputPortName = newElement('div', `customMidiOutputPortName_${outputPort.id}`, 'port-name');
+            //outputPortName.innerHTML = ` id:${outputPort.id} manufacturer: ${outputPort.manufacturer} name: ${outputPort.name} version:${outputPort.version}`;
+            outputPortName.innerHTML = `${outputPort.manufacturer} ${outputPort.name}`;
+            outputPortPanel.appendChild(outputPortName);
+            // toggle button for connecting
+            const outputPortToggle = newElement('button', `outputPortToggle_${outputPort.id}`, 'port-toggle toggled-off');
+            outputPortToggle.innerHTML = "Connect";
+            outputPortPanel.appendChild(outputPortToggle);
+            if (outputPort.connection == 'open') {
+              outputPortToggle.classList.remove('toggled-off');
+              outputPortToggle.classList.add('toggled-on');
+              outputPortToggle.innerHTML = "Disconnect";
+            }
+            outputPortToggle.addEventListener('click', async (event) => {
+              if (outputPortToggle.classList.contains('toggled-on')) {
+                await outputPort.close();
+                outputPortToggle.classList.remove('toggled-on');
+                outputPortToggle.classList.remove('toggled-off');
+                outputPortToggle.innerHTML = 'Connect';
+              } else {
+                await outputPort.open();
+                outputPortToggle.classList.remove('toggled-off');
+                outputPortToggle.classList.add('toggled-on');
+                outputPortToggle.innerHTML = 'Disconnect';
+              }          
+            });
+          });
+        }
+      }
+    }
+
+    connectedCallback() {
+      console.log('MIDI custom element added to page.');
+      //populatePortTables(this);
+      this.init();
+    }
+  
+    disconnectedCallback() {
+      console.log('MIDI custom element removed from page.');
+    }
+  
+    adoptedCallback() {
+      console.log('MIDI custom element moved to new page.');
+    }
+
+    async init() {
+      this.midi = await navigator.requestMIDIAccess();
+      this.populatePortTables();
+    }
+  
+}
+  
+customElements.define('custom-midi', CustomMIDI);
