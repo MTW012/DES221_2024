900da0e2919d107c669391761eb3bf7e4e907853
dr-offig <dr.offig@offig.com>
2023-02-21 13:39:56 +1000

    Removed example interface, replaced with placeholder template

--- index.js
+++ index.js
@@ -9,31 +9,3 @@ function sendStringToMicrobit(str) {
 }
 
 // put any javascript you need for your interface here
-// Get the buttons and slider elements
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const slider = document.getElementById("mySlider");
-
-// Add event listeners to the buttons
-button1.addEventListener("click", function() {
-  console.log("Heart Button clicked");
-  sendStringToMicrobit("heart");
-});
-
-button2.addEventListener("click", function() {
-  console.log("Diamond button clicked!");
-  sendStringToMicrobit("diamond");
-});
-
-button3.addEventListener("click", function() {
-  console.log("Square button clicked!");
-  sendStringToMicrobit("square");
-});
-
-// Add event listener to the slider
-slider.addEventListener("input", function() {
-  console.log("Slider value changed to " + slider.value);
-  sendStringToMicrobit(`sensitivity ${slider.value}`);
-});
-
