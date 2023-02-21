2301133ced5f2c4c7b09bb68470e471cf725451f
dr-offig <dr.offig@offig.com>
2023-02-21 16:04:17 +1000

    Added README and cleaned up some old commented out code

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // It adds a "newline" character at the end of the string, so that the microbit
 // program can tell the command is complete
 function sendStringToMicrobit(str) {
-    const serialComponent = document.querySelector('custom-serial');
-    if (serialComponent) {
-        serialComponent.writeToSerial(`${str}\n`);
-    }
+  const serialComponent = document.querySelector('custom-serial');
+  if (serialComponent) {
+    serialComponent.writeToSerial(`${str}\n`);
+  }
 }
 
 // put any javascript you need for your interface here
